1dba8d048602e878b77665e6c1f96f97bd5109ea
zeng <zeng92922@163.com>
2019-06-24 22:33:39 +0800

    重构springBoot

--- src/main/java/com/study/study_space/controller/HelloWorldController.java
+++ src/main/java/com/study/study_space/controller/HelloWorldController.java
@@ -1,24 +1,34 @@
-package com.study.study_space.web.controller;
+package com.study.study_space.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String  gethtml(){
-        return  "helloWorld";
+        return  "helloWorld.html";
     }
 
-    //什么时候会返回html   什么时候会返回jsp
-
+    //form表单的提交
     @RequestMapping(value = "/save",method = RequestMethod.GET)
     public String  getaaa(String username,String password){
         System.out.println(username);
         System.out.println(password);
        return "aaa";
+    }
+
+    //跳转包括视图和数据
+    @RequestMapping("/Hi")
+    public ModelAndView sayHello() {
+           ModelAndView modelAndView = new ModelAndView();
+          modelAndView.setViewName("test1");
+          modelAndView.addObject("key", 12345);
+          //System.out.println("test");
+          return modelAndView;
     }
 
 }
