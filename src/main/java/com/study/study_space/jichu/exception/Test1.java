f2379d0e12cc18705be77b5e0b21892d80c5a536
zeng <zeng92922@163.com>
2019-08-08 11:28:22 +0800

    1.0

--- src/main/java/com/study/study_space/jichu/exception/Test1.java
+++ src/main/java/com/study/study_space/jichu/exception/Test1.java
@@ -1,4 +1,4 @@
-package jichu.exception;
+package com.study.study_space.jichu.exception;
 
 import java.io.IOException;
 
@@ -6,19 +6,25 @@ public class Test1 {
 
     public static void main(String[] args){
 
-        getException();
-
+        getExceptionLog();
     }
 
-    public static void getException(){
+    public static void getExceptionLog(){
 
         try {
             throw new IOException();
         } catch (IOException e) {
+            //printStackTrace  和  toString  打印日志的详细程度不同
             e.printStackTrace();
+            System.out.println("-------------------------------------");
+            System.out.println(e.toString());
+            System.out.println("-------------------------------------");
+            //看源码知道  因为这里没有有参的构造函数初始化 message  所以这里为null
+            String message = e.getMessage();
+            System.out.println(message);
         }
 
+        //将异常catch以后  后面的代码可以继续执行
         System.out.println("hahaha");
-
     }
 }
--- src/main/java/com/study/study_space/test/aaa.java
+++ src/main/java/com/study/study_space/test/aaa.java
@@ -1,4 +1,13 @@
 package com.study.study_space.test;
 
 public class aaa {
+
+    public static void main(String[] args) {
+
+        ThreadLocal t=new ThreadLocal();
+        t.set("aaa");
+
+
+    }
+
 }
