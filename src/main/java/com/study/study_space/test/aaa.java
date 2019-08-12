bcd3cd2bac26bc29d63c11d5a81bc44e6aad3674
zeng <zeng92922@163.com>
2019-08-12 15:05:48 +0800

    1.0

--- src/main/java/com/study/study_space/test/aaa.java
+++ src/main/java/com/study/study_space/test/aaa.java
@@ -1,5 +1,9 @@
 package com.study.study_space.test;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class aaa {
 
     public static void main(String[] args) {
@@ -7,7 +11,19 @@ public class aaa {
         ThreadLocal t=new ThreadLocal();
         t.set("aaa");
 
-        System.out.println(123==123);
+//        ThreadLocal<SimpleDateFormat>  format2  = new ThreadLocal<SimpleDateFormat>(){
+//            protected SimpleDateFormat initialValue() {
+//                SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//                return f;
+//            }
+//        };
+
+
+
+         Object object=new Object(){};
+
+        ArrayList<String> list = new ArrayList<String>() {{}};
+
 
 
     }
