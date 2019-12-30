c31518f7afd8beefaf5cb1ed4288fa227803ac70
0113557 <zengguanghao@bonc.com.cn>
2019-12-30 15:00:41 +0800

    aaa

--- src/main/java/com/study/study_space/shujujiegou/tree/ErXTree.java
+++ src/main/java/com/study/study_space/shujujiegou/tree/ErXTree.java
@@ -2,48 +2,50 @@ package com.study.study_space.shujujiegou.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ErXTree {
 
     public static void main(String[] args) {
 
         Node node = initDate();
-        depthPriority(node);
-
+//        depthPriority(node);
+        widthPriority(node);
 
     }
 
 
-    public static Node initDate(){
+    public static Node initDate() {
 
         Node resultNode;
 
-        Node  aNode=new Node();
+        Node aNode = new Node();
         aNode.setValue("A");
         aNode.setChildren(new ArrayList<>());
 
-        Node  bNode=new Node();
+        Node bNode = new Node();
         bNode.setValue("B");
         bNode.setChildren(new ArrayList<>());
 
-        Node  cNode=new Node();
+        Node cNode = new Node();
         cNode.setValue("C");
         cNode.setChildren(new ArrayList<>());
 
-        Node  dNode=new Node();
+        Node dNode = new Node();
         dNode.setValue("D");
         dNode.setChildren(new ArrayList<>());
 
-        Node  eNode=new Node();
+        Node eNode = new Node();
         eNode.setValue("E");
         eNode.setChildren(new ArrayList<>());
 
-        Node  fNode=new Node();
+        Node fNode = new Node();
         fNode.setValue("F");
         fNode.setChildren(new ArrayList<>());
 
-        Node  gNode=new Node();
+        Node gNode = new Node();
         gNode.setValue("G");
         gNode.setChildren(new ArrayList<>());
 
@@ -56,25 +58,38 @@ public class ErXTree {
         cNode.getChildren().add(fNode);
         cNode.getChildren().add(gNode);
 
-        resultNode=aNode;
+        resultNode = aNode;
 
         return resultNode;
     }
 
     //利用栈先进后出的特性  进行深度遍历
-    public static void  depthPriority(Node node) {
-
+    public static void depthPriority(Node node) {
         Stack<Node> stack = new Stack();
         stack.push(node);
-
         while (!stack.isEmpty()) {
-
             Node top = stack.pop();
             System.out.println(top.getValue());
             List<Node> children = top.getChildren();
-            if (!children.isEmpty()&& children !=null){
-                for (int i = children.size() - 1; i >= 0; i--){
+            if (!children.isEmpty() && children != null) {
+                for (int i = children.size() - 1; i >= 0; i--) {
                     stack.push(children.get(i));
+                }
+            }
+        }
+    }
+
+    //利用队列先进先出的特性  进行深度遍历
+    public static void widthPriority(Node node) {
+        Queue<Node> queue = new LinkedBlockingQueue<Node>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node first = queue.poll();
+            System.out.println(first.getValue());
+            List<Node> children = first.getChildren();
+            if (children != null && children.size() > 0) {
+                for (int i = 0; i < children.size(); i++) {
+                    queue.add(children.get(i));
                 }
             }
         }
--- src/main/java/com/study/study_space/test/Test2.java
+++ src/main/java/com/study/study_space/test/Test2.java
@@ -1,32 +1,17 @@
 package com.study.study_space.test;
 
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import com.study.Test1;
 
-@Configuration
 public class Test2 {
 
     public static void main(String[] args){
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(Test2.class);
-        Test3 test3 = (Test3)context.getBean("testBean");
-        test3.sayHello();
+
+        Test1.Ceshi();
 
     }
 
 
-    public Test2(){
-        System.out.println("hello world");
-    }
-
-
-    @Bean
-    public Test3 testBean() {
-        return new Test3();
-    }
 
 }
