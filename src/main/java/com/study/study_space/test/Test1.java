263f9e1d6753598dd61b5bad4a5a2e016f92a51f
zengguanghao <zengguanghao@bonc.com.cn>
2020-05-06 11:37:22 +0800

    清理

--- src/main/java/com/study/study_space/test/Test1.java
+++ src/main/java/com/study/study_space/test/Test1.java
@@ -4,27 +4,6 @@ public class Test1 {
 
     public static void main(String[] args) {
 
-        System.out.println("lllllllllllllllllll");
-
-
-        System.out.println("git reset demo");
-
-        System.out.println("ceshi");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        System.out.println("heihei");
 
     }
 
