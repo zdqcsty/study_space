e8bd7ae47dda4335ac557c07bf28e0648d3d1714
ceshi <zeng92922@163.com>
2020-09-22 15:07:03 +0800

    JAXB 解析XML样例

---
+++ src/main/java/com/study/study_space/jichu/data/jaxb/JaxbReadXml.java
@@ -0,0 +1,80 @@
+package com.study.study_space.jichu.data.jaxb;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.MessageFormat;
+import java.util.List;
+
+public class JaxbReadXml {
+
+    @SuppressWarnings("unchecked")
+    public static <T> T readString(Class<T> clazz, String context) throws JAXBException, IOException {
+        try {
+            InputStream inputStream = ClassLoader.getSystemResource(context).openStream();
+            JAXBContext jc = JAXBContext.newInstance(clazz);
+            Unmarshaller u = jc.createUnmarshaller();
+            return (T) u.unmarshal(inputStream);
+        } catch (JAXBException e) {
+            throw e;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T readConfig(Class<T> clazz, String config, Object... arguments) throws IOException,
+            JAXBException {
+        InputStream is = null;
+        try {
+            if (arguments.length > 0) {
+                config = MessageFormat.format(config, arguments);
+            }
+            // logger.trace("read configFileName=" + config);
+            JAXBContext jc = JAXBContext.newInstance(clazz);
+            Unmarshaller u = jc.createUnmarshaller();
+            is = new FileInputStream(config);
+            return (T) u.unmarshal(is);
+        } catch (IOException e) {
+            // logger.trace(config, e);
+            throw e;
+        } catch (JAXBException e) {
+            // logger.trace(config, e);
+            throw e;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T readConfigFromStream(Class<T> clazz, InputStream dataStream) throws JAXBException {
+        try {
+            JAXBContext jc = JAXBContext.newInstance(clazz);
+            Unmarshaller u = jc.createUnmarshaller();
+            return (T) u.unmarshal(dataStream);
+        } catch (JAXBException e) {
+            // logger.trace(e);
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        InputStream inputStream = ClassLoader.getSystemResource("./datafile/clustersinfo.xml").openStream();
+        JAXBContext jc = JAXBContext.newInstance(ClusterInfoList.class);
+        Unmarshaller u = jc.createUnmarshaller();
+        ClusterInfoList list = (ClusterInfoList) u.unmarshal(inputStream);
+
+        List<ClusterInfo> clusterinfos = list.getClusterinfos();
+        for (ClusterInfo info : clusterinfos) {
+            XmlHdfsInfo hdfsInfo = info.getHdfsInfo();
+            System.out.println(info.getName());
+            System.out.println(hdfsInfo.getKeytabPath());
+        }
+
+    }
+
+}
---
+++ src/main/java/com/study/study_space/jichu/data/jaxb/XmlHdfsInfo.java
@@ -0,0 +1,79 @@
+package com.study.study_space.jichu.data.jaxb;
+
+import javax.xml.bind.annotation.*;
+
+
+public class XmlHdfsInfo {
+
+    @XmlElement(name = "hdfsconfpath")
+    private String hdfsConfPath;
+    @XmlElement(name = "hdfsuri")
+    private String hdfsUri;
+    @XmlElement(name = "user")
+    private String user;
+    @XmlElement(name = "krb5confpath")
+    private String krb5ConfPath;
+    @XmlElement(name = "userkerberos")
+    private String userKerberos;
+    @XmlElement(name = "keytabpath")
+    private String keytabPath;
+
+    public XmlHdfsInfo() {
+    }
+
+
+    @XmlTransient
+    public String getHdfsConfPath() {
+        return hdfsConfPath;
+    }
+
+    public void setHdfsConfPath(String hdfsConfPath) {
+        this.hdfsConfPath = hdfsConfPath;
+    }
+
+    @XmlTransient
+    public String getHdfsUri() {
+        return hdfsUri;
+    }
+
+    public void setHdfsUri(String hdfsUri) {
+        this.hdfsUri = hdfsUri;
+    }
+
+    @XmlTransient
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @XmlTransient
+    public String getKrb5ConfPath() {
+        return krb5ConfPath;
+    }
+
+    public void setKrb5ConfPath(String krb5ConfPath) {
+        this.krb5ConfPath = krb5ConfPath;
+    }
+
+    @XmlTransient
+    public String getUserKerberos() {
+        return userKerberos;
+    }
+
+    public void setUserKerberos(String userKerberos) {
+        this.userKerberos = userKerberos;
+    }
+
+    @XmlTransient
+    public String getKeytabPath() {
+        return keytabPath;
+    }
+
+    public void setKeytabPath(String keytabPath) {
+        this.keytabPath = keytabPath;
+    }
+
+}
